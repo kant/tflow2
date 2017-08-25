1ce7266b8f5f92cff9e2607c4a4d3740471adab1
Julian Kornberger <jk+github@digineo.de>
2017-08-25 03:48:50 +0200

    Increase test coverage

--- database/map_tree_test.go
+++ database/map_tree_test.go
@@ -12,6 +12,7 @@ func TestCreateKey(t *testing.T) {
 
 	assert.Equal("test", createKey("test"))
 	assert.Equal("\x17", createKey(byte(23)))
+	assert.Equal("\x00\x17", createKey(uint16(23)))
 	assert.Equal("\x17\x2a", createKey([]uint8{23, 42}))
 	assert.Equal("\x00\x00\x00\x00\x00\x00\x00\x17", createKey(int64(23)))
 	assert.Equal("\x00\x00\x00\x17", createKey(uint32(23)))
@@ -19,4 +20,14 @@ func TestCreateKey(t *testing.T) {
 	assert.Equal("\xfe\x80\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01", createKey(net.ParseIP("fe80::1")))
 
 	assert.PanicsWithValuef("unsupported key type: struct {}", func() { createKey(struct{}{}) }, "")
+}
+
+func TestMapTree(t *testing.T) {
+	assert := assert.New(t)
+
+	mapTree := newMapTree()
+	assert.Nil(mapTree.Get("foo"))
+
+	mapTree.Insert("foo", "bar")
+	assert.NotNil(mapTree.Get("foo"))
 }
